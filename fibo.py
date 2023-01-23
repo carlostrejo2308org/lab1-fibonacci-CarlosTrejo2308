ab31a13e2be7c3c946178617882fa343edfe56b2
Carlos N. Trejo <14352052+CarlosTrejo2308@users.noreply.github.com>
2023-01-23 11:27:31 -0600

    feat(fibo): improve fibo to make it iterative

--- fibo.py
+++ fibo.py
@@ -1,9 +1,15 @@
 
 def fibo(x):
-  if x <= 1:
-    return x
-  
-  return fibo(x-1) + fibo(x-2)
+    if x <= 0:
+        return x
+
+    ln = [0, 1]
+
+    while x > 1:
+        ln[0], ln[1] = ln[1], ln[0] + ln[1]
+        x -= 1
+    return ln[1]
+
 
 # Get input
 x = int(input())
